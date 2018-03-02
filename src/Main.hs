3d0b25c8a6c0f8b17c71fb7bbceb72b4a953a9c7
James Burton <j.burton@brighton.ac.uk>
2018-03-02 11:30:35 +0000

    Tidied up pure version

--- src/Main.hs
+++ src/Main.hs
@@ -5,28 +5,20 @@ import Data.Maybe (fromJust)
 data Coin = Pound | Fifty | Twenty | Ten | Five | Two | Penny
                deriving (Eq, Ord, Enum, Show)
 
-{-makeChange :: Int -> Int -> Maybe (Int, [Change])
-makeChange cost paid | cost > paid = Nothing
-                     | cost `div` 100 > 0 = Just ()
--}
-
 values :: [(Coin, Int)]
 values = zip [Pound .. Penny] [100, 50, 20, 10, 5, 2, 1]
 
-getCoin :: Int -> Coin
-getCoin i = fst $ head $ dropWhile ((>i) . snd) values
+getCoin :: Int -> (Coin, Int)
+getCoin i = head $ dropWhile ((>i) . snd) values
 
-coinDiv :: Int -> Coin -> ([Coin], Int)
-coinDiv n c = let i     = fromJust $ lookup c values
-                  (d,m) = n `divMod` i in
-              if n < i then coinDiv n (succ c) else (replicate d c, m)
+coinDiv :: Int -> (Coin, Int) -> ([Coin], Int)
+coinDiv n (c,i) = let (d,m) = n `divMod` i in
+                  (replicate d c, m)
 
 makeChange :: Int -> [Coin]
 makeChange 0 = []
-makeChange n = let c = getCoin n
-                   (cs, rem) = coinDiv n c
-                   cs' = makeChange rem in
-               cs ++ cs'
+makeChange n = let (cs, rem) = coinDiv n (getCoin n) in
+               cs ++ (makeChange rem)
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
