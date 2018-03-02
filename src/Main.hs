2d1f5236ba5345f4020cc97af01115658f7b8450
Jim Burton <j.burton@brighton.ac.uk>
2018-03-02 16:13:03 +0000

    Added comments.

--- src/Main.hs
+++ src/Main.hs
@@ -3,16 +3,27 @@ module Main where
 data Coin = Pound | Fifty | Twenty | Ten | Five | Two | Penny
                deriving (Eq, Ord, Enum, Show)
 
+{-| Pairs each coin with its monetary value in pennies.  -}
 values :: [(Coin, Int)]
 values = zip [Pound .. Penny] [100, 50, 20, 10, 5, 2, 1]
 
+{-| Finds the highest denomination coin that is smaller than the argument. -}
 getCoin :: Int -> (Coin, Int)
 getCoin i = head $ dropWhile ((>i) . snd) values
 
+{-| The first argument, n, is a number of pennies, while the second argument, (c, i),
+    is a pair of a coin and its monetary value. The return value is a list of coins
+    where each coin is equal to c and the length of the list is equal to the largest
+    number of c coins that is less than n, paired with the remainder after the value of
+    these coins is subtracted from n.
+-}
 coinDiv :: Int -> (Coin, Int) -> ([Coin], Int)
 coinDiv n (c,i) = let (d,m) = n `divMod` i in
                   (replicate d c, m)
 
+{-| Takes a number of pennies and returns the shortest list of coins that make up
+    that value.
+-}
 makeChange :: Int -> [Coin]
 makeChange 0 = []
 makeChange n = let (cs, rem) = coinDiv n (getCoin n) in
